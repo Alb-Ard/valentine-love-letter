03945640e798082566c663e6f8516eb3309a0e15
Alberto Arduini <alberto.arduini3@studio.unibo.it>
2024-02-13 11:54:10 +0100

    Added letter.it domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://alb-ard.github.io',
-    base: '/valentine-love-letter',
+    site: 'http://letter.it',
   })
