0740aa7a415eabaf140dea4d17edfeda627a8e18
Alberto Arduini <alberto.arduini3@studio.unibo.it>
2024-02-13 11:41:35 +0100

    Updated github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://alb-ard.github.io',
+    base: '/valentine-love-letter',
+  })
